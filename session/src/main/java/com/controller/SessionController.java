fd76ea01167545a58c295a9cc4baff466c653b5f
sahil <sahiillx02@gmail.com>
2024-06-28 17:24:20 +0530

    spring web session + send data + read data

--- session/src/main/java/com/controller/SessionController.java
+++ session/src/main/java/com/controller/SessionController.java
@@ -2,23 +2,40 @@ package com.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.bean.UserBean;
 
 @Controller
 public class SessionController {
 
+	// login method
 	@GetMapping("/login")
 	public String login() {
 		return "login.jsp";
 	}
 
+	// signup method
 	@GetMapping("/signup")
 	public String signup() {
 		return "signup.jsp";
 	}
-	
+
+	// forgetpassword method
 	@GetMapping("/forgetPassword")
 	public String forgetPassword() {
 		return "forgetPassword.jsp";
+	}
+
+	// save method
+	@PostMapping("/saveuser")
+	public String saveUser(UserBean userBean) {
+		
+		System.out.println(userBean.getName());
+		System.out.println(userBean.getEmail());
+		System.out.println(userBean.getPassword());
+		
+		return "login.jsp";
 	}
 
 }
--- session/src/main/webapp/signup.jsp
+++ session/src/main/webapp/signup.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,5 +8,12 @@
 </head>
 <body>
 	<h1>Singup Page</h1>
+
+	<form action="saveuser" method="post">
+		User name : <input type="text" name="name"> <br>
+		<br>Email : <input type="email" name="email"> <br>
+		<br>Password : <input type="password" name="password"> <br>
+		<br> <input type="submit" value="Submit">
+	</form>
 </body>
 </html>
